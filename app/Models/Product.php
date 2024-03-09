f04cd889e8d9769251a411102af6c3eac0920ca9
Matthew Dawkins <matthew.dawkins@gmail.com>
2024-03-09 22:02:38 +0000

    Add model validation

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,10 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Category;
+use Watson\Validating\ValidatingTrait;
 
 class Product extends Model
 {
     use HasFactory;
+    use ValidatingTrait;
 
     protected $fillable = [
         'code',
@@ -24,6 +26,15 @@ class Product extends Model
     protected $keyType = 'string';
 
     public $incrementing = false;
+
+    protected $rules = [
+        'code' => ['required', 'string'],
+        'name' => ['required', 'string'],
+        'price_ex_vat' => ['required', 'numeric'],
+        'price_inc_vat' => ['required', 'numeric'],
+        'stock' => ['required', 'integer'],
+        'short_description' => ['string'],
+    ];
 
     public function category()
     {
